b175a71397dfb80eb1806cca93e1dd4369eb47a3
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-12-12 21:10:57 +0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -7,10 +7,10 @@ export async function all(m) {
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
             if (this.spam[m.sender].count > 5) {
                 global.db.data.users[m.sender].banned = true
-                let bear = `*📮Kamu di banned karena spam...!!!*
+                let bear = `*📮You are banned because of spam...!!!*
 
 _*Note*_
-Mau di unban? Silahkan subscribe like dan koment terlebih dahulu di video ini https://youtu.be/h8lX25IPgr4 lalu screnshoot dan kirim buktinya ke saya agar saya unban`
+Want to unban? If you want to be unban do nothing and just chill`
                 m.reply(bear)
             }
             this.spam[m.sender].count = 0
