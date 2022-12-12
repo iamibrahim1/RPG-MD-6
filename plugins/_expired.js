463743fcb0e439faa39561f4eabb44c757c63fbb
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-12-12 21:13:00 +0500

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -3,7 +3,7 @@ handler.before = async function (m) {
 
     if (m.isGroup && global.db.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.db.data.chats[m.chat].expired) {
-            this.reply(m.chat, `? waktunya *${this.user.name}* untuk meninggalkan grup\n? Jangan lupa sewa lagi ya!`, null).then(() => {
+            this.reply(m.chat, `? waktunya *${this.user.name}* to leave the group\n? Don't forget to rent again!`, null).then(() => {
                 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         global.db.data.chats[m.chat].expired = 0
